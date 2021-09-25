3467ebf5fccf1383c762f2a26e3915bd6a22fc3a
aneog007 <aneogi007@gmail.com>
2021-09-25 02:14:22 -0400

    Gathering Asset Info

--- TestFiles/NFTapi.js
+++ TestFiles/NFTapi.js
@@ -20,5 +20,28 @@ request(options, function (error, response) {
   console.log("this is the asset image url:", image_url);
 });
 
- 
+var request = require('request');
+var options = {
+  'method': 'GET',
+  'url': 'https://api.opensea.io/api/v1/assets?owner=0x38d920102cbf2b37c57792859d894bc3d5522dd2&order_direction=desc&offset=0&limit=20',
+  'headers': {
+    'Cookie': '__cf_bm=eaAs5sUd5uNn_4YTsxSyzi1rSuM54pnCL1TDeRmP9dc-1632530585-0-AYNkhhXDxjotBsdfPG5V+ucLYSnWnxozOpFxmwkISQ17BVRuYKvvhhuwv8uG4DSGhG+9nX2t3zFwpFxIchCH4TM='
+  }
+};
 
+request(options, function (error, response) {
+  if (error) throw new Error(error);
+
+  parsedBody = JSON.parse(response.body);
+
+  for (let i = 0; i < parsedBody.assets.length; i++) {
+
+    currAsset = parsedBody.assets[i];
+
+    console.log('Asset[',i,'] Name:', currAsset.name);
+    console.log('           ImageUrl:', currAsset.image_url);
+    console.log('           CreatorUserName:', currAsset.creator.user.username);
+
+  }
+  
+});
