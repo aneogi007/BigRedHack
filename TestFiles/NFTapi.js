d364e30f4c5805c24985893dc25eca2b9f1b4587
aneog007 <aneogi007@gmail.com>
2021-09-25 05:32:38 -0400

    Gathering Asset Info

--- TestFiles/NFTapi.js
+++ TestFiles/NFTapi.js
@@ -1,29 +1,12 @@
 //install npm install request
 
+UserId = '0x38d920102cbf2b37c57792859d894bc3d5522dd2'
+
 var request = require('request');
 var options = {
-  'method': 'GET',
-  'url': 'https://api.opensea.io/api/v1/asset/0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb/1/',
-  'headers': {
-    'Cookie': '__cf_bm=eaAs5sUd5uNn_4YTsxSyzi1rSuM54pnCL1TDeRmP9dc-1632530585-0-AYNkhhXDxjotBsdfPG5V+ucLYSnWnxozOpFxmwkISQ17BVRuYKvvhhuwv8uG4DSGhG+9nX2t3zFwpFxIchCH4TM='
-  }
-};
-
-var image_url = 'none';
-
-request(options, function (error, response) {
-  if (error) throw new Error(error);
-
-  parsedBody = JSON.parse(response.body);
-  image_url = parsedBody.image_url; 
   
-  console.log("this is the asset image url:", image_url);
-});
-
-var request = require('request');
-var options = {
   'method': 'GET',
-  'url': 'https://api.opensea.io/api/v1/assets?owner=0x38d920102cbf2b37c57792859d894bc3d5522dd2&order_direction=desc&offset=0&limit=20',
+  'url': 'https://api.opensea.io/api/v1/assets?owner='+UserId+'&order_direction=desc&offset=0&limit=50',
   'headers': {
     'Cookie': '__cf_bm=eaAs5sUd5uNn_4YTsxSyzi1rSuM54pnCL1TDeRmP9dc-1632530585-0-AYNkhhXDxjotBsdfPG5V+ucLYSnWnxozOpFxmwkISQ17BVRuYKvvhhuwv8uG4DSGhG+9nX2t3zFwpFxIchCH4TM='
   }
@@ -37,10 +20,20 @@ request(options, function (error, response) {
   for (let i = 0; i < parsedBody.assets.length; i++) {
 
     currAsset = parsedBody.assets[i];
+    
+    if (currAsset.name) {
+      console.log('Asset[',i,'] Name:', currAsset.name);
+      console.log('           ImageUrl:', currAsset.image_url+'=s0');
+      console.log('           OwnerUserName:', currAsset.owner.user.username); 
 
-    console.log('Asset[',i,'] Name:', currAsset.name);
-    console.log('           ImageUrl:', currAsset.image_url);
-    console.log('           CreatorUserName:', currAsset.creator.user.username);
+      if (currAsset.creator && currAsset.creator.user.username ) {
+        console.log('           CreatorUserName:', currAsset.creator.user.username);
+      } else {
+        console.log('           CreatorUserName: unknown');
+      }
+    }
+    
+   
 
   }
   
